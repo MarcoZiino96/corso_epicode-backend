a0ad96f237b0d40237fcabba6c7f4d65455e5010
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-01-08 17:58:08 +0100

    first commit

---
+++ src/it/epicode/week1/esercizio_day1/esercizio3.java
@@ -0,0 +1,21 @@
+package it.epicode.week1.esercizio_day1;
+import java.util.Scanner;
+
+public class esercizio3 {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+
+
+
+    public static double perRettangolo (double x, double y){
+
+        return (x+y)*2;
+    }
+}
