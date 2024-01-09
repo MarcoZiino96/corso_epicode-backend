7f6aab44ee0b7722e21bcec01dbb0a2efbfaff0e
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-01-09 15:11:57 +0100

    esercizio 3 complete

--- src/it/epicode/week1/esercizio_day2/Esercizio3.java
+++ src/it/epicode/week1/esercizio_day2/Esercizio3.java
@@ -1,4 +1,31 @@
 package it.epicode.week1.esercizio_day2;
 
+import java.util.Scanner;
+
 public class Esercizio3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero compreso tra 0 e 3");
+
+        int x = scanner.nextInt();
+
+        switch (x){
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("uno");
+                break;
+            case 2:
+                System.out.println("due");
+                break;
+            case 3:
+                System.out.println("tre");
+                break;
+            default:
+                System.out.println("Errore:il numero deve essere compreso tra 0 e 3");
+        }
+}
 }
